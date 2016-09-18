784678312807460205e3424b2dd2fbc79abc59e0
marymini <speckymini@gmail.com>
2016-09-18 11:40:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/Article1',function(req,res){
-   res.send('Article One') ;
+  res.sendFile(path.join(__dirname, 'ui', 'Article1.html'));
 });
 app.get('/Article2',function(req,res){
    res.send('Article Two') ;
