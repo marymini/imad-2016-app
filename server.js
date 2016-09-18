43a644f738257b75b9102701c137eb9c39424c51
marymini <speckymini@gmail.com>
2016-09-18 11:04:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('Article1',function(req,res){
+   res.send('Article One') ;
+});
+app.get('Article2',function(req,res){
+   res.send('Article Two') ;
+});
+app.get('Article3',function(req,res){
+   res.send('Article Three'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
