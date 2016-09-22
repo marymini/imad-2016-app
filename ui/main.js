88f44034520cdfb27874a930766b27c1b941ec37
marymini <speckymini@gmail.com>
2016-09-22 13:56:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element=document.getElementByID('txt');
 element.innerhtml="The Text Value Changed";
 var img=document.getElementByID('madi');
-img.onclick=function() {
+img.onClick=function() {
     img.style.marginleft='100px';
     };
