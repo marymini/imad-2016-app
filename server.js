dafee75875f8e5ea767c6240e27ac44db9271244
marymini <speckymini@gmail.com>
2016-09-23 22:01:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,14 +94,16 @@ function createTemplate(data) {
    `;
     return htmlTemplate;
 }
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 var counter=0;
 app.get('/counter',function(req,res){
    counter=counter+1;
    re.send(counter.toString()); 
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
