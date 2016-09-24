7fe674f2ce83d9a44c32f335e6a41262a6dd8aec
marymini <speckymini@gmail.com>
2016-09-24 19:45:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,27 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
+    
+    //Create a Request Object
+    var request=new XMLHttpRequest();
+    
+    
+    
+    //Capture the response and store it in a variable
+    
+    request.onreadystatechange=function() {
+        if(request.readystate==XMLHttpRequest.DONE) {
+            if(request.status==200) {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                sapn.innerHTML=counter.toString();
+            }
+        }
+    };
+    
+    // Make Request
+    
+    
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
