b42b72bf21a5485a388aab46c7b382ee6f0a1762
marymini <speckymini@gmail.com>
2016-09-25 15:07:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,15 @@ button.onclick=function(){
     request.open('GET','http://marymini.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function() {
+    var names=['name1','name2','name3'];
+    for(var i=0;i<names.length;i++) {
+        list+='<li>'+name[i]+'</li>';
+            }
+var ul=document.getElementById('namelist');
+ul.innerHTML=list;
+};
