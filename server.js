536604335e0a069da1071af654b69d7dfe6618d4
marymini <speckymini@gmail.com>
2016-11-06 19:16:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,72 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config={
+    user:'marymini',
+    database:'marymini',
+    host:'db.imad.hasura-app.io',
+    password:DB-PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmlTemplate=`
+    <html>
+    <head>
+    <title>
+    ${title}
+    </title>
+    <link href="ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+    <div class="container">
+    <div>
+    <a href="/">HOME</a>
+    </div>
+    <hr/>
+    <h3>
+    ${heading}
+    </h3>
+    <div>
+    ${date}
+    </div>
+    <div>
+    ${content}
+    </div>
+    </body>
+    </html>  `;
+    
+    return htmlTemplate;
+    
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool=new Pool(config);
+app.get('/articles/:articleName',function(req,res){
+    pool.query("select * from article where title=$1",[req.params.articleName],function(err,result){
+        if(err){
+            res.status(500).send(err.tostring());
+                    } else {
+                        if(result.length===0){
+                            res.status(404).send("Not Found");
+                                                    }else{
+                                                        var articleData=result.rows[0];
+                                                        res.send(createTemplate(articleData));
+                                                    }
+                    }
+    });
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
